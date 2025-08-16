ba6a78ce29a393abe3444b991c31152ef21088a6
sathish-strategyfox <kannaasathish@gmail.com>
2025-08-17 01:56:31 +0530

    Initial commit

---
+++ src/app/builder/page.tsx
@@ -0,0 +1,10 @@
+import styles from "../page.module.css";
+
+export default function Builder() {
+  return (
+    <div className={styles.page}>
+      <h1>Builder Page</h1>
+      <p>Welcome to the builder route. Start building your room configuration here.</p>
+    </div>
+  );
+}
